e0829b6f2644d6ef57916331885c884e222dcb78
Victor Vrabie <victor.v.vrabie@gmail.com>
2017-01-17 19:22:11 +0100

    2 Funtions

--- Roulette.py
+++ Roulette.py
@@ -6,7 +6,7 @@ random.seed(3456)
 def AboveMinimum (bet, minbet):
     output = []
     for item in bet:
-        output.append(bool(item>bet))
+        output.append(bool(item>minbet))
     print(output)
 
 
@@ -15,9 +15,23 @@ def SpinTheWheel (bets):
     output = []
     for item in bets:
         output.append(bool(item == winnumb))
-    print(" The winning number is " + str(winnumb))
+    print(" Ball lands on " + str(winnumb))
     if sum(output) > 0:
-        print(" The number of winners is " + str(sum(output)))
+        print(" There are " + str(sum(output)) + "correct bet(s)")
     else:
-        print("There are no winners")
+        print("No winners this round")
 
+
+amounts1 = [10, 85, 120, 65, 150, 122]
+bets1 = [10, 24, 36, 0, 11, 24]
+class Roulette(object):
+    def __init__(self,Roulette):
+        self.Roulette = Roulette
+    def Roulette(minbet):
+        Roulette.Roulette= AboveMinimum(amounts1,minbet)
+
+    def SimulateGame(bet,amount):
+        Roulette.Roulette.SimulateGame = SpinTheWheel(bet)
+
+table1 = Roulette.Roulette(100)
+print(table1.SimulateGame(bets1,amounts1))
