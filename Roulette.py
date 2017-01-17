c98d907b807a73bf6c1ab88ce7c06db5f88180d7
Victor Vrabie <victor.v.vrabie@gmail.com>
2017-01-17 17:23:25 +0100

    AboveMinimum

--- Roulette.py
+++ Roulette.py
@@ -1,1 +1,9 @@
-#ia ibal in cur la cal
+
+def AboveMinimum (bet, minbet):
+    output = []
+    for item in bet :
+        if item > minbet:
+            output.append("TRUE")
+        else :
+            output.append("FALSE")
+    print(output)
