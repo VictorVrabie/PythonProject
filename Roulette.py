74f6f80a9480b177e5faf5ec4e902e3d45f96ee1
Victor Vrabie <victor.v.vrabie@gmail.com>
2017-01-17 17:50:25 +0100

    AboveMinimum

--- Roulette.py
+++ Roulette.py
@@ -2,20 +2,22 @@ import random
 # This is used to fixethe random generator so we can test the output
 random.seed(3456)
 
+
 def AboveMinimum (bet, minbet):
     output = []
     for item in bet:
         output.append(bool(item>bet))
     print(output)
 
+
 def SpinTheWheel (bets):##
     winnumb=random.randint(0,36)
     output = []
     for item in bets:
-        output.append(bool(item==winnumb))
+        output.append(bool(item == winnumb))
     print(" The winning number is " + str(winnumb))
-    if sum(output)>0:
-        print(" The number of winers is " + str(sum(output)) )
+    if sum(output) > 0:
+        print(" The number of winners is " + str(sum(output)))
     else:
-        print("There are no winers")
+        print("There are no winners")
 
