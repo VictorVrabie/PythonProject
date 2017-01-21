76cb9b4515d217e1d6d528b8a4945f2ff1d8bcc4
Victor Vrabie <victor.v.vrabie@gmail.com>
2017-01-21 11:52:37 +0100

    Craps

--- Roulette.py
+++ Roulette.py
@@ -2,35 +2,35 @@ import random
 # This is used to fixe the random generator so we can test the output
 random.seed(3456)
 
+class Roulette:
+    def __init__(self,min):
+        self.min = min
 
-def AboveMinimum (bet, minbet):
-    output = []
-    for item in bet:
-        output.append(bool(item>= minbet))
-    return(output)
+    def SimulateGame(self, bet, amount):
 
-def SpinTheWheel (bets):
-    winnumb=random.randint(0,36)
-    output = []
-    for item in bets:
-        output.append(bool(item == winnumb))
-    print(" Spinning the wheel...")
-    print(" Ball lands on " + str(winnumb))
-    if sum(output) > 0:
-        print(" There are " + str(sum(output)) + " correct bet(s)")
-    else:
-        print("No winners this round")
-    return (output)
+        def AboveMinimum (amount):
+            output = []
+            for item in amount:
+                output.append(bool(item >= self.min))
+            return(output)
 
-class Roulette(object):
+        def SpinTheWheel (bet):
+            winnumb=random.randint(0,36)
+            output = []
+            for item in bet:
+                output.append(bool(item == winnumb))
+            print(" Spinning the wheel...")
+            print(" Ball lands on " + str(winnumb))
+            if sum(output) > 0:
+                print(" There are " + str(sum(output)) + " correct bet(s)")
+            else:
+                print("No winners this round")
+            return (output)
 
-    def Roulette(minbet):
-        Roulette.Roulette = minbet
 
-    def SimulateGame(minbet,bet,amount):
         vec = []
         gain = 0
-        a = AboveMinimum(amount, minbet)
+        a = AboveMinimum(amount)
         s = SpinTheWheel(bet)
         for item in range(len(amount)):
             if a[item]*s[item]==0:
@@ -40,11 +40,6 @@ class Roulette(object):
                 vec.append(amount[item]*30)
         return ([gain, vec])
 
-
-amounts1 = [10, 85, 120, 65, 150, 122]
-bets1 = [10, 24, 36, 0, 11, 24]
-print(Roulette.SimulateGame(100,bets1,amounts1))
-print(Roulette.SimulateGame(100,bets1,amounts1))
 
 
 
---
+++ trah.py
@@ -0,0 +1,7 @@
+class Complex:
+    def __init__(self, minbet):
+        self.Roulette = minbet
+
+x = Complex(3.0)
+
+print(x.Roulette)
