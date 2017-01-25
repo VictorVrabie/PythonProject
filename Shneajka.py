0cbd2618e25f7b34151901734a46025c80741298
Victor Vrabie <victor.v.vrabie@gmail.com>
2017-01-25 18:17:55 +0100

    Simulation

---
+++ Shneajka.py
@@ -0,0 +1,37 @@
+import random
+class Customer:
+    def __init__(self, wealth, bet):
+        self.wealth = wealth
+        self.bet = bet
+
+    def drinks(self):
+        if self.wealth > 60:
+            self.drink = random.randint(1,2)*20
+            self.tips = random.randint(0,20)
+            self.wealth -= self.drink+self.tips
+            return True
+        else :
+            self.drink = 0
+            self.tips = 0
+            return False
+
+class Returning(Customer):
+    def rich(self):
+        self.wealth = random.randint(100,300)
+        self.bet = 0
+        return False
+
+class OneTime(Customer):
+    def rich(self):
+        self.wealth = random.randint(200,300)
+        self.bet = random.randint(0,(self.wealth/3))
+        return False
+
+class Bachelor(Customer):
+    def rich(self):
+        self.wealth = random.randint(200,500) + 0
+        self.bet = random.randint(0,self.wealth)
+        return False
+
+
+
